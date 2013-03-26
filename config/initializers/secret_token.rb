adaffdf44740095dd2c8199d5bc81d90667a7a96
Marek Ryniejski <marekjulian@gmail.com>
2013-03-25 18:52:44 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marekjulian3TstNoframework::Application.config.secret_token = 'ed001bc60c5554a327db2a6f8066ca6e9273394e5bca68a13987fd7645b309b6a72fbe17edfa1697d6556f00ee156f04c47acd6bdbf9c20f2960d13c883ad930'
